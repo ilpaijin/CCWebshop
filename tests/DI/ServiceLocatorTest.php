9455157fc1deb072dd20f81b1083efb5e9848954
ilpaijin <ilpaijin@gmail.com>
2014-01-14 20:22:47 +0100

    phpunit config

---
+++ tests/DI/ServiceLocatorTest.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+* ServiceLocatorTest class
+*
+* @package default
+* @author ilpaijin <ilpaijin@gmail.com>
+*/
+class ServiceLocatorTest extends PHPUnit_Framework_TestCase
+{
+    public function testItWorks()
+    {
+        $this->assertTrue(true);
+    }
+}
