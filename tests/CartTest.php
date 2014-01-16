133322457fa40455e2716d5fdda6b6e56f8b322f
ilpaijin <ilpaijin@gmail.com>
2014-01-16 12:24:13 +0100

    CartTest remove product added

--- tests/CartTest.php
+++ tests/CartTest.php
@@ -37,7 +37,9 @@ class CartTest extends PHPUnit_Framework_TestCase
     public function testCanAddProductToCache()
     {
 
-        $result = array();
+        $result = array(
+            'name' => 'Paio', //SR Principle 
+        );
 
         $this->caching
             ->shouldReceive('getContents')
@@ -51,6 +53,69 @@ class CartTest extends PHPUnit_Framework_TestCase
 
         $this->cart->addProduct($this->mockProduct);
 
-        $this->assertEquals(array(), $this->cart->getCachedContents());
+        $this->assertEquals($result, $this->cart->getCachedContents());
+    }
+
+    public function testCanRemoveProductFromCache()
+    {
+        $result = array('name' => 'John Doe');
+
+        $this->mockProduct = \Mockery::mock('Webshop\Products\Product');
+        $this->anotherMockProduct = \Mockery::mock('Webshop\Products\Product');
+
+        $this->caching
+            ->shouldReceive('add')
+            ->with($this->mockProduct)
+            ->once();
+
+        $this->caching
+            ->shouldReceive('add')
+            ->with($this->anotherMockProduct)
+            ->once();    
+
+        $this->caching
+            ->shouldReceive('remove')
+            ->with($this->mockProduct)
+            ->once();    
+
+        $this->caching
+            ->shouldReceive('getContents')
+            ->once()
+            ->andReturn($result);    
+
+        $this->cart->addProduct($this->mockProduct);
+        $this->cart->addProduct($this->anotherMockProduct);
+        $this->cart->removeProduct($this->mockProduct);
+
+        $this->assertEquals($result, $this->cart->getCachedContents());
+    }
+
+    public function testCalculateTotal()
+    {
+        $return = "<h3> Cached Total: $10.00</h3>";
+
+        $this->caching
+            ->shouldReceive('add')
+            ->with($this->mockProduct)
+            ->once(); 
+
+        $this->mockProduct
+            ->shouldReceive('getPrice')
+            ->once()
+            ->andReturn(10)
+            ->shouldReceive('getQty')
+            ->once()
+            ->andReturn(1);            
+        
+        $contents = array($this->mockProduct);
+
+        $this->caching
+            ->shouldReceive('getContents')
+            ->once()
+            ->andReturn($contents);     
+
+var_dump($this->cart->getCachedContentsTotal());
+
+        $this->assertEquals($return, $this->cart->getCachedContentsTotal());
     }
 }
